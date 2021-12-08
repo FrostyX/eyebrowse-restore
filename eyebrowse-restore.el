0bef76b0ef9f87964e5159d120be784f07812b7e
Jakub Kadlcik <frostyx@email.cz>
2021-12-08 22:21:03 +0100

    Untabify everything

--- eyebrowse-restore.el
+++ eyebrowse-restore.el
@@ -12,51 +12,51 @@
   (interactive)
   (make-directory eyebrowse-restore-dir t)
   (dolist (frame (frame-list))
-	(eyebrowse-restore-save-window-config frame)))
+    (eyebrowse-restore-save-window-config frame)))
 
 
 (defun eyebrowse-restore-save-window-config (frame)
   (let* ((name (frame-parameter frame 'name))
-		 (path (concat (file-name-as-directory eyebrowse-restore-dir) name))
-		 (window-configs (eyebrowse--get 'window-configs frame)))
+         (path (concat (file-name-as-directory eyebrowse-restore-dir) name))
+         (window-configs (eyebrowse--get 'window-configs frame)))
 
-	(with-temp-file path
-	  (prin1 window-configs (current-buffer))))
+    (with-temp-file path
+      (prin1 window-configs (current-buffer))))
   (eyebrowse-restore--remove-unused-backups))
 
 
 (defun eyebrowse-restore-restore ()
   (interactive)
   (let* ((name (completing-read
-				"Eyebrowse backups: "
-				(eyebrowse-restore--list-backups)))
-		 (path (concat (file-name-as-directory eyebrowse-restore-dir) name)))
+                "Eyebrowse backups: "
+                (eyebrowse-restore--list-backups)))
+         (path (concat (file-name-as-directory eyebrowse-restore-dir) name)))
 
-	(with-temp-buffer
+    (with-temp-buffer
       (insert-file-contents path)
-	  (eyebrowse--set 'window-configs
-		(read (buffer-string))))))
+      (eyebrowse--set 'window-configs
+        (read (buffer-string))))))
 
 
 (defun eyebrowse-restore--list-backups ()
   (seq-filter
    (lambda (x)
-	 (not (member x '("." ".."))))
+     (not (member x '("." ".."))))
    (directory-files eyebrowse-restore-dir)))
 
 
 
 (defun eyebrowse-restore-unused-backup-p (name)
   (not (member
-		name
-		(mapcar (lambda (x) (frame-parameter x 'name))
-				(frame-list)))))
+        name
+        (mapcar (lambda (x) (frame-parameter x 'name))
+                (frame-list)))))
 
 
 (defun eyebrowse-restore--remove-unused-backups ()
   (dolist (name (eyebrowse-restore--list-backups))
-	(if (eyebrowse-restore-unused-backup-p name)
-		(delete-file (concat (file-name-as-directory eyebrowse-restore-dir) name)))))
+    (if (eyebrowse-restore-unused-backup-p name)
+        (delete-file (concat (file-name-as-directory eyebrowse-restore-dir) name)))))
 
 
 
